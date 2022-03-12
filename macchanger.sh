68be31b453518a170a022c47c5c6dd9e6779a9b4
Cristian Cezar Moisés <86272521+cristiancmoises@users.noreply.github.com>
2022-03-12 17:55:11 -0300

    Create macchanger.sh

---
+++ macchanger.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "                                                                                                                          "
+echo -e "                \e[1;31m *---------------------* \e[0m"
+printf "           \e[1;31m|\e[0m    \033[1;32mWelcome Hacker\033[0m   \e[1;31m|\e[0m\n"
+printf "               \e[1;31m|\e[0m\033[1;32m Have a Nice Day! \033[0m\e[1;31m|\e[0m\n"
+echo -e "          \e[1;31m *---------------------*\e[0m"
+printf "                      \e[1;31m/                       \ \e[0m \n"
+printf "         \e[1;31m/             \ \e[0m \n"
+day=$(date +%A)
+User=$(whoami)
+echo -e " \e[1;37m ------------------                     ----------------\e[0m"
+echo -e " \e[1;37m|\e[0m\033[6mToday is : $day\033[0m\e[1;37m|\e[0m\e[1;37m         |\e[0m \033[6mUser Is : $User\033[0m\e[1;37m |\e[0m"
+echo -e " \e[1;37m ------------------          ----------------\e[0m"
+echo -e "           *_____________________*"
+echo -e "                 |\033[1;32mYour Wifi Mac-Address\033[0m|"
+echo "*----------------------*---------------------*"
+ifconfig wlan0 down
+macchanger -r wlan0
+echo "*--------------------------------------------*"
+echo "          *_________________________*"
+echo -e "          |\033[1;32mYour Ethernet Mac-Address\033[0m|"
+echo "*---------------------*-------------------------*"
+macchanger -r wlan0
+ifconfig wlan0 up
+
+echo "*-----------------------------------------------*"
+echo -e "                 *\033[1;32m!!..Yup That's..!!\033[0m*"
+echo -e "     *\033[1;32mType macchanger --show enp6s0 OR macchanger --show wlan0\033[0m*"
